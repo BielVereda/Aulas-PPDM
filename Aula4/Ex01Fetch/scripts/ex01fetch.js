61cd120cbb79d61fb9d1b7e4774dfcd833e99996
BielVereda <gabrielsantosvereda@gmail.com>
2026-02-19 11:10:12 -0300

    Corrige ID da lista de usuários no HTML e implementa exibição dinâmica de usuários no JavaScript

--- Aula4/Ex01Fetch/scripts/ex01fetch.js
+++ Aula4/Ex01Fetch/scripts/ex01fetch.js
@@ -7,3 +7,18 @@ fetch("https://jsonplaceholder.typicode.com/users/")
 
 // Converter a resposta no formato JSON para um objeto JS
 .then(response => response.json())
+
+// Com o JSON convertido, vamos exibir as informações na página
+.then(usuario => {
+    const todosUsuarios = document.getElementById("listaUsuarios")
+
+    usuario.forEach(user => {
+        const cadaUsuario = document.createElement("li")
+        cadaUsuario.textContent = user.name + " (" + user.email + ")";
+        todosUsuarios.appendChild(cadaUsuario);
+    });
+})
+
+.catch(error => {
+    document.getElementById("todosUsuarios").textContent = "Erro ao buscar usuários"
+})
